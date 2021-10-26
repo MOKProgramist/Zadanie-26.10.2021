82cfb3c05fd6d1f3864b00d3672c6c42624dffcd
MOKProgramist <is24romanovar@artcollege.ru>
2021-10-26 20:57:42 +0300

    Test
    Test

---
+++ Калькулятор простой/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Калькулятор_простой
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, ok;
+            string oper; // знак операции 
+
+            Console.Write("Введите первый операнд: ");
+            x = Convert.ToDouble(Console.ReadLine()); // считываем знач консоли
+
+            Console.Write("Введите знак операции: ");
+            oper = Console.ReadLine(); // считываем знач консоли
+
+            Console.Write("Введите второй операнд: ");
+            y = Convert.ToDouble(Console.ReadLine()); // считываем знач консоли
+
+            switch (oper) // передаем значение и выполняем действия далее
+            {
+                case "+":
+                    ok = Math.Round(x + y, 3); // вычислыем и сокращаем
+                    Console.WriteLine($"x{oper}y = {ok}");
+                    break;
+                case "-":
+                    ok = Math.Round(x - y, 3); // вычислыем и сокращаем
+                    Console.WriteLine($"x{oper}y = {ok}");
+                    break;
+                case "*":
+                    ok = Math.Round(x * y, 3); // вычислыем и сокращаем
+                    Console.WriteLine($"x{oper}y = {ok}");
+                    break;
+                case "/":
+                    if (y == 0)
+                    {
+                        Console.WriteLine("На ноль делить нельзя!");
+                        return;
+                    }
+                    ok = Math.Round(x / y, 3); // вычислыем и сокращаем
+                    Console.WriteLine($"x{oper}y = {ok}");
+                    break;
+                default:
+                    Console.WriteLine("Оператор не найден, ошибка");
+                    break;
+            }
+
+            Console.ReadLine(); // задерживаем консоль
+        }
+
+    }
+}
---
+++ Нахождение функции 2.0/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Нахождение_функции_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+
+            Console.Write("Введите число: ");
+            x = Convert.ToDouble(Console.ReadLine()); // считываем знач консоли
+
+            if (x < 0) {
+                y = 1; // задаем знач.
+            }
+            else if ((0 <= x) && (x < 1)) {
+                y = 2; // задаем знач
+            }
+            else {
+                y = 3;
+            }
+
+            switch (y) // передаем значение и выполняем действия далее
+            {
+                case 1:
+                    y = (Math.Pow((Math.Pow(x, 3) + 1), 2)); // вычисляем и сокращаем
+                    Console.WriteLine($"y = {y}");
+                    break;
+                case 2:
+                    y = 0; // вычисляем и сокращаем
+                    Console.WriteLine($"y = {y}");
+                    break;
+                case 3:
+                    y = Math.Abs(Math.Pow(x, 2) - (5 * x) + 1); // вычислыем и сокращаем
+                    Console.WriteLine($"y = {y}");
+                    break;
+                default:
+                    Console.WriteLine("Ошибка!");
+                    break;
+            }
+        }
+    }
+}
